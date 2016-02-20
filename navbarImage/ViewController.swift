f3ad384eee90fdf00cdc639e29ba75c52668beb5
Andrew Seeley <awseeley@gmail.com>
2016-02-21 10:55:09 +1100

    Swift Navigation Bar Image Tutorial
    Swift Navigation Bar Image Tutorial

---
+++ navbarImage/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  navbarImage
+//
+//  Created by Andrew on 18/02/2016.
+//  Copyright © 2016 Seemu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let navBackgroundImage = UIImage(named: "gummi_bear_navbar") // Get our image
+        self.navigationController!.navigationBar.setBackgroundImage(navBackgroundImage, forBarMetrics: .Default) // Set the Nav Bar Image
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
